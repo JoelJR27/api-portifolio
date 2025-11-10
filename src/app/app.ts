a90b80b13aadd8b644097766872c7535db8dfe4a
Joel Júnior <joelviana250@gmail.com>
2025-11-10 11:02:34 -0300

    alterar cors

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,11 +7,11 @@ import cookieParser from "cookie-parser";
 const app: Application = express();
 
 app.use(cookieParser(), cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://localhost:3001"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-}),)
+},));
 
 routes(app);
 

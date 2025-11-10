ce51bd0811b94cb852959489674ba924beea8510
Joel Júnior <joelviana250@gmail.com>
2025-11-09 22:17:57 -0300

    deploy prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,7 @@
-import prisma from "../src/database.js"
+import { PrismaClient } from "@prisma/client"
 import hashData from "../src/libs/hashData.js"
 
+const prisma = new PrismaClient()
 const hashedPassword = await hashData(process.env.ADMIN_USER_PASSWORD!)
 async function main() {
     const joel = await prisma.user.upsert({

5ee254f7b3eccd2924fa740801b3c0b48a2247be
Joel Júnior <joelviana250@gmail.com>
2025-11-09 21:13:38 -0300

    alterar local prisma client

--- src/database.ts
+++ src/database.ts
@@ -1,3 +1,4 @@
-import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaClient } from "@prisma/client";
+
 const prisma: PrismaClient = new PrismaClient();
 export default prisma;
